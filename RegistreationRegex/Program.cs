0ddf49042b2ca55d6622c09a51a4c24fd27f011a
PratibhaMastud <karandepratibha999@gmail.com>
2020-12-05 10:53:43 +0530

    [Pratibha Mastud] Add: Welcome in User Registration

---
+++ RegistreationRegex/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace RegistreationRegex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration");
+            int i = 0;
+            while (i != 1)
+            {
+                Console.WriteLine("Enter First Name");
+                string first_name = Console.ReadLine();
+                Boolean output = UserRegistration.regexFirstName(first_name);
+                if (output == true)
+                {
+                    Console.WriteLine("Valid firstName");
+                    i = 1;
+                }
+                else
+                {
+                    Console.WriteLine("Please Enter valid First Name starting with capital letter and should have minimum 3 Character in the string");
+                }
+            }
+        }
+    }
+}
+   
+
---
+++ RegistreationRegex/UserRegistration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+
+
+namespace RegistreationRegex
+{
+    class UserRegistration
+    {
+        public static bool regexFirstName(string first_name)
+        {
+            String namePattern = "^[A-Z]{1}[a-z]{2,}";
+            Regex obj = new Regex(namePattern);
+            return obj.IsMatch(first_name);
+        }
+    }
+}
