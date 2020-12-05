49fe472c0f83db29b8955795f75d3bcd84a93189
PratibhaMastud <karandepratibha999@gmail.com>
2020-12-05 11:30:21 +0530

    [Pratibha Mastud] Add: Enter valid Email in this format abc.xyz@bl.co.in

--- RegistreationRegex/Program.cs
+++ RegistreationRegex/Program.cs
@@ -10,17 +10,17 @@ namespace RegistreationRegex
             int i = 0;
             while (i != 1)
             {
-                Console.WriteLine("Enter Last Name");
-                string first_name = Console.ReadLine();
-                Boolean output = UserRegistration.regexFirstName(first_name);
+                Console.WriteLine("Enter Email");
+                string email = Console.ReadLine();
+                Boolean output = UserRegistration.regexFirstName(email);
                 if (output == true)
                 {
-                    Console.WriteLine("Valid LastName");
+                    Console.WriteLine("Valid email");
                     i = 1;
                 }
                 else
                 {
-                    Console.WriteLine("Please Enter valid Last Name starting with capital letter and should have minimum 3 Character in the string");
+                    Console.WriteLine("Enter email in this format : abc.xyz@co.in");
                 }
             }
         }
--- RegistreationRegex/UserRegistration.cs
+++ RegistreationRegex/UserRegistration.cs
@@ -11,7 +11,7 @@ namespace RegistreationRegex
     {
         public static bool regexFirstName(string last_name)
         {
-            String namePattern = "^[A-Z]{1}[a-z]{2,}";
+            String namePattern = "^([a-z]{3,})([.]{0,1}[a-z]*)@([a-z]{2}).([a-z]{2})([.]{1}[a-z]{2}){0,1}$";
             Regex obj = new Regex(namePattern);
             return obj.IsMatch(last_name);
         }
